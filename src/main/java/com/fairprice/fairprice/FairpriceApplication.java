d7049bd1657ed63390a021e8703679be2eaa83a9
humphrey <humphreymutuma01@gmail.com>
2024-11-07 13:24:25 +0300

    initial setup

---
+++ src/main/java/com/fairprice/fairprice/FairpriceApplication.java
@@ -0,0 +1,14 @@
+package com.fairprice.fairprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairpriceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FairpriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fairprice/fairprice/FairpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairprice.fairprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
