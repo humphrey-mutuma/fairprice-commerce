35023d5506f1c01af21c546a2107faf39da40b3d
humphrey <humphreymutuma01@gmail.com>
2024-11-08 16:47:12 +0300

    Fix swagger CORS

--- src/main/java/com/fairprice/fairprice/config/OpenApiConfig.java
+++ src/main/java/com/fairprice/fairprice/config/OpenApiConfig.java
@@ -1,5 +1,6 @@
 package com.fairprice.fairprice.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -7,24 +8,16 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.Components;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityScheme.Type;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("FairPrice APIs").version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearer-key")) // Apply bearer-key scheme
+                .addServersItem(new Server().url("https://fairprice.up.railway.app")) // Replace with your HTTPS URL
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key", new SecurityScheme()
                                 .type(Type.HTTP)
